75bf4327208952b883dac38fc36192cbf1f2e4e9
karthi <karthikeyanks463@gmail.com>
2025-05-06 00:37:57 +0530

    Fix Analysis Bugs

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"; // Import SpeedInsights
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 // Context Providers
 import { AuthProvider } from "@/context/AuthContext";
@@ -61,7 +61,7 @@ const App = () => (
         </AuthProvider>
       </BrowserRouter>
       <Analytics /> {/* Add Analytics here */}
-      <SpeedInsights /> {/* Add SpeedInsights here */}
+      <SpeedInsights /> {/* Add Speed Insights here */}
     </TooltipProvider>
   </QueryClientProvider>
 );
